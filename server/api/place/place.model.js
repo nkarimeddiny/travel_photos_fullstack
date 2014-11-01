db5e4b09c0fd869d5e3358b6a7e0725ff21ca106
Navid Karimeddiny <nkarimeddiny@gmail.com>
2014-11-01 12:47:39 -0400

    added place api files

---
+++ server/api/place/place.model.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var PlaceSchema = new Schema({
+   user: {type: Schema.Types.ObjectId, ref: "User"},
+   date: { type: Date, default: Date.now },
+   location: String,
+   text: String,
+   latitude : String,
+   longitude : String
+});
+
+module.exports = mongoose.model('Place', PlaceSchema);
