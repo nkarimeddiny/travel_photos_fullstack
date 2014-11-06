2ba2308c9c3432658e3bd924d1182484c7c8159e
Navid Karimeddiny <nkarimeddiny@gmail.com>
2014-11-06 17:29:55 -0500

    sidebar.html cleaned up and commented

--- client/components/sidebar/sidebar.controller.js
+++ client/components/sidebar/sidebar.controller.js
@@ -2,82 +2,41 @@
 
 var app = angular.module('travelPhotosApp');
 
-app.controller('FriendslistCtrl', function ($scope, $state, $http, $location, friendsListService) {
-   $scope.message = 'Hello';
+app.controller('FriendslistCtrl', function ($scope, $state, $http, 
+                                      $location, friendsListService) {
    var ctrl = this;
    ctrl.myFriendsList = [];
    ctrl.signedUpUsers = [];
    ctrl.friendsOrderObject = {};
 
+   this.removeFriend = function(friendName) {
+      friendsListService.removeFriend(friendName, ctrl, $http);
+   }
+
+   this.addFriend = friendsListService.addFriend;
+
+///////////////////////////////////////////////////
+//for angular/jquery-ui sortable:
    ctrl.sortableOptions = {
       'ui-floating': true,
       stop: function( event, ui ) {
         friendsListService.updateFriendsOrder(ctrl, $http);
       } 
    };
-  
+///////////////////////////////////////////////////////   
+
     var friendSearch = $("#friendSearch");
 
     friendsListService.initializeSidebar(friendSearch, ctrl, $http);
+
+/////////////////////////////////////////////
+//when the search form is used, the user can click on a friend's
+//name, and the addFriend method is called:
 
     $(".ui-widget").on("click", ".ui-menu-item", function(event){
       ctrl.addFriend($(event.target).text(), ctrl, $http);
     });
 
-
-    this.removeFriend = function(friendName) {
-       friendsListService.removeFriend(friendName, ctrl, $http);
-    }
-
-    this.addFriend = friendsListService.addFriend;
+/////////////////////////////////////////////    
 
 });
-
-// app.factory("friendsListService", function() {
-//     return {
-//       addFriend : function(friend, ctrl, $http) {
-//           $http.post("api/users/addFriend", {
-//                 friend: friend
-//                 })
-//              .success( function(data) {
-//                 console.log(data.userFriends);
-//                 ctrl.myFriendsList = data.userFriends; 
-//                }); 
-//           },
-//       removeFriend : function(friendName, ctrl, $http) {
-//           $http.post("api/users/removeFriend", {
-//                 friendName: friendName
-//                 })
-//              .success( function(data) {
-//                 ctrl.myFriendsList = data.userFriends; 
-//                }); 
-//           },
-//       updateFriendsOrder : function(ctrl, $http) {
-//           ctrl.myFriendsList.forEach(function(friend, index){
-//              ctrl.friendsOrderObject[friend.name] = index + 1;
-//           });
-//          $http.post("api/users/updateFriendsOrder", {friendsOrder : ctrl.friendsOrderObject})
-//              .success( function(data) {
-//                  console.log(data);
-//                  console.log(data.userFriends);
-//                  ctrl.myFriendsList = data.userFriends; 
-//         });
-//       },
-//       initializeSidebar: function(friendSearch, ctrl, $http) {
-//            $http.get("api/users/sideBarInfo")
-//              .success( function(data) {
-//               console.log(data);
-//                   ctrl.thisUserName = data.username;
-//                   ctrl.myFriendsList = data.userFriends; 
-//                   ctrl.signedUpUsers = data.users
-
-//                   friendSearch.autocomplete({
-//                    minLength: 3,
-//                    source: ctrl.signedUpUsers,
-//                    position: { my: "left top", at: "left bottom" },
-//                    appendTo: ".ui-widget"
-//                  });
-//              });
-//       }
-//     }
-//   });
