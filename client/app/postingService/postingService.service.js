74fdb00dc91a7de84da2206bb6b1ea5c37f6e106
Navid Karimeddiny <nkarimeddiny@gmail.com>
2014-11-06 17:20:01 -0500

    comments added to postingService retrievePlaces and removePlace methods

--- client/app/postingService/postingService.service.js
+++ client/app/postingService/postingService.service.js
@@ -42,18 +42,29 @@ angular.module('travelPhotosApp')
                       googleMapsService.initialize(ctrl);
               });
        },
+
+       //retrievePlaces method is called by main controller, and sends a 
+       //request to the server for the current user's saved places-to-go.
+       //It assigns myPlacesList to data.placesToGo, which is an array of 
+       //objects for each place-to-go that has been saved by the current user.
+       //The googleMapsService.initialize method is then called in order to 
+       //add markers to the map 
         retrievePlaces : function($http, ctrl, googleMapsService) {
               $http.get("api/users/retrievePlaces") 
                   .success(function(data) {
                       ctrl.myPlacesList = data.placesToGo;
-                      console.log(ctrl.myPlacesList);
                       googleMapsService.initialize(ctrl);
               });
        },
+
+       //removePlace method is called by main controller, and takes an id 
+       //for a saved place-to-go. It sends the placeId to the server, which
+       //deletes the place-to-go, then assigns myPlacesList to the returned
+       //data, which is an array of objects for each place-to-go that has been
+       //saved by the current user
         removePlace : function(placeId, $http, ctrl) {
               $http.post("api/users/removePlace", {placeId : placeId}) 
                   .success(function(data) {
-                      console.log(data);
                       ctrl.myPlacesList = data;
               });
        }
