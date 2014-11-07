3ab5fdab2bbf519469703faa9d56dbf502982a60
Navid Karimeddiny <nkarimeddiny@gmail.com>
2014-11-06 20:35:59 -0500

    comments added to myPosts controller

--- client/app/myPosts/myPosts.controller.js
+++ client/app/myPosts/myPosts.controller.js
@@ -6,36 +6,47 @@ angular.module('travelPhotosApp')
     
     var ctrl = this;
     this.userPosts = {};
-    this.lowResImageIds = {};
+    this.lowResImageIds = {};  //lowResImageIds is used to 
+    //determine whether or not an image retrieved from Instagram
+    //has already been posted
     this.thumbnailImages = {};
-    this.thumbnailImagesIsEmpty = true;
+    this.thumbnailImagesIsEmpty = true; //thumbnailImagesIsEmpty 
+    //is used to determine whether or not to show the button for
+    //retrieving Instagram images
+    this.isInstagramUser;  //isInstagramUser is used to determine 
+    //whether or not to show the button for retrieving Instagram images
 
     postingService.retrievePosts($http, ctrl);
 
     var currentUser = Auth.getCurrentUser();
     this.isInstagramUser = (currentUser.provider === 'instagram');
 
-    this.addPost = function(imageLink, caption, instagramLink, imageId) {
-       postingService.addPost(imageLink, caption, instagramLink, imageId, ctrl, $http);
+    this.addPost = function(imageLink, caption, 
+                            instagramLink, imageId) {
+       postingService.addPost(imageLink, caption, instagramLink, 
+                              imageId, ctrl, $http);
     };
+
     this.removePost = function(postId) {
        postingService.removePost($http, ctrl, postId);
     };
 
     this.accessInstagram = function() {
         $http.get("api/users/accessInstagram")
-             .success( function(data) {
-                 data.data.forEach(function(post, index){
-                     if (!ctrl.lowResImageIds[post.id]) {
-                         ctrl.thumbnailImages[index] = 
-                           {id : post.id,
-                            instagramLink : post.link,
-                            lowresLink: post.images.low_resolution.url,
-                            thumbnailLink: post.images.thumbnail.url,
-                            caption : post.caption.text};
-                     }
-                 });
-                 ctrl.thumbnailImagesIsEmpty = $.isEmptyObject(ctrl.thumbnailImages);
+          .success( function(data) {
+            data.data.forEach(function(post, index) { 
+              if (!ctrl.lowResImageIds[post.id]) {
+               //if the image retrieved from Instagram hasn't
+               //already been posted: 
+                ctrl.thumbnailImages[index] = 
+                   {id : post.id,
+                   instagramLink : post.link,
+                   lowresLink: post.images.low_resolution.url,
+                   thumbnailLink: post.images.thumbnail.url,
+                   caption : post.caption.text};
+               }
+            });
+            ctrl.thumbnailImagesIsEmpty = $.isEmptyObject(ctrl.thumbnailImages);
         });
     };
 
