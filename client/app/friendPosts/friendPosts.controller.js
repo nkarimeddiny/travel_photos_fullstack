4368bb88d5ad5734bdcf7f923c817ed46a36ee1c
Navid Karimeddiny <nkarimeddiny@gmail.com>
2014-11-09 19:30:27 -0500

    only three of friend's photos are shown at first. Click a button to show three more

--- client/app/friendPosts/friendPosts.controller.js
+++ client/app/friendPosts/friendPosts.controller.js
@@ -3,7 +3,12 @@
 angular.module('travelPhotosApp')
   .controller('FriendpostsCtrl', 
   	  function ($scope, $stateParams, postingService, $http) {
-    var ctrl = this;
-    this.userPosts = {}
-    postingService.retrieveFriendPosts($http, ctrl, $stateParams.friendName);
+        var ctrl = this;
+        this.userPosts = {}
+        postingService.retrieveFriendPosts($http, ctrl, $stateParams.friendName);
+    
+        this.displayNum = 3;
+        this.increaseDisplayNum = function() {
+        	ctrl.displayNum += 3;
+        }
 }); 
